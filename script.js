ed075083ac3a238a73ca5403c95c0e8dcfc0c7e4
Anishraj96 <122950580+Anishraj96@users.noreply.github.com>
2025-07-24 20:22:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -104,4 +104,18 @@ function checkWinner(player) {
 
 function checkWinnerFor(b, player) {
   const wins = [
-    [0,1,2], [3,4,5], [6,7,8]()
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+  ];
+  return wins.some(combo => combo.every(i => b[i] === player));
+}
+
+function resetGame() {
+  board = ["", "", "", "", "", "", "", "", ""];
+  gameOver = false;
+  statusElement.textContent = "Your turn (X)";
+  renderBoard();
+}
+
+renderBoard();
